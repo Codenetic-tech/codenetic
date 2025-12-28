47bf06bd52a6d0cbfdbbbcadf9305ed262a97c21
Codenetic-tech <codenetic.tech@gmail.com>
2025-12-28 15:34:07 +0500

    created one doctype meta settings

---
+++ codenetic/codenetic/doctype/meta_settings/meta_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, codenetic tech solution and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Meta Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
